278d6a811e46e247192ca5032c039b8f979da822
tsuji <>
2018-01-28 19:06:38 +0900

    fix model and migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :events
+  resources :events
+  resources :events
 
   root to: 'welcome#index'
   get '/auth/:provider/callback' => 'sessions#create'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180125112218) do
+ActiveRecord::Schema.define(version: 20180128093146) do
 
   create_table "events", force: :cascade do |t|
     t.integer  "owner_id"
